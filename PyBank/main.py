7f8ac01978519df67027fe2ff17bd3ef5b7fe980
Brett Williams <brettwilliams@Bretts-MacBook-Pro.local>
2020-10-12 18:14:06 -0700

    starting night of hw

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,10 @@
 import os
 import csv
+
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    for row in csvreader:
+       totalcount = totalcount +1
