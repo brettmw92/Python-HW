ffad3f6ef9176582b0d1ad0df4c177eba7b79c36
Brett Williams <brettwilliams@Bretts-MacBook-Pro.local>
2020-10-17 20:58:30 -0700

    upload python hw try 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,6 @@ import csv
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-
-    for row in csvreader:
-       totalcount = totalcount +1
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Date', 'Profit'])
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,8 +3,47 @@ import csv
 
 csvpath = os.path.join("Resources", "election_data.csv")
 
+total_votes = 0
+candidates = {}
+
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile,delimiter = ",")
+
+    next(csvreader)
 
     for row in csvreader:
-       totalcount = totalcount +1
+        total_votes += 1
+
+        if candidates.get(row[2]):
+            candidates[row[2]] +=1
+            
+        else:
+            candidates[row[2]]=1
+
+print("----------------")
+print("Election Results")
+print("----------------")
+print(f"Total votes: {total_votes}")
+print("----------------")
+# print(candidates)
+# print(candidates.keys())
+
+winning_vote=0
+winning_candidate=""
+
+for candidate_name, votes in candidates.items():
+   # print(candidate_name)
+   # print(votes)
+    percentage = (votes/total_votes)*100
+    percentagerounded = round(percentage,5)
+   # print(f"{percentage}%")
+    print(f"{candidate_name}:  {percentagerounded}%  {votes}")
+
+    if votes > winning_vote:
+        winning_vote = votes
+        winning_candidate=candidate_name
+
+print("----------------")
+print(f"{winning_candidate} is the winning candidate")
+print(f"{winning_vote} was the total vote count for {winning_candidate}")
+print("----------------")
