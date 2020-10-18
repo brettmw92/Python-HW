3d8e6415432c1c8b8f479e665483d140ca7a215a
Brett Williams <brettwilliams@Bretts-MacBook-Pro.local>
2020-10-17 22:58:15 -0700

    started PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,32 @@ import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
+months = 0
+net_total = 0
+greatestincrease = 0
+greatestdecrease = 0
+
 with open(csvpath) as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',')
-    csvwriter.writerow(['Date', 'Profit'])
+    csvreader = csv.reader(csvfile,delimiter = ",")
+
+    next(csvreader)
+
+    for row in csvreader:
+        months = months+1
+        # net_total = net_total + row[2]
+
+
+print(f"Total Months: {months}")
+print(f"Total: {net_total}")
+averagechange = net_total/months
+print(f"Average Change: {averagechange}")
+
+print(f"Greatest Increase in Profits: {greatestincrease}")
+print(f"Greatest Decrease in Profits: {greatestdecrease}")
+
+
+# output_file = os.path.join("analysis", "analysis.txt")
+# with open(csvpath) as csvfile:
+#     csvwriter = csv.writer(csvfile, delimiter=',')
+#     csvwriter.writerow(['Date', 'Profit'])
         
