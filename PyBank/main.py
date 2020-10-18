9b756d9a2223d3544f8bdd89fa656a17e9252655
Brett Williams <brettwilliams@Bretts-MacBook-Pro.local>
2020-10-18 00:15:57 -0700

    submitting Python challenge hw

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 title = []
 date = []
 profit = []
-months = -1
+months = 0
 net_total = 0
 greatestincrease = 0
 greatestdecrease = 0
@@ -14,20 +14,18 @@ greatestdecrease = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile,delimiter = ",")
+    csvheader = next(csvreader)
+
     for row in csvreader:
         months += 1
         profit.append(row[1])
         date.append(row[0])
 
-print(profit)
+greatestincrease = max(profit)
+greatestdecrease = min(profit)
 
-    # if candidates.get(row[2]):
-    #         candidates[row[2]] +=1
-            
-    #     else:
-    #         candidates[row[2]]=1
-
-
+print(f"Financial Analysis")
+print(f"-------------------")
 print(f"Total Months: {months}")
 print(f"Total: {net_total}")
 averagechange = net_total/months
@@ -40,5 +38,5 @@ print(f"Greatest Decrease in Profits: {greatestdecrease}")
 # output_file = os.path.join("analysis", "analysis.txt")
 # with open(csvpath) as csvfile:
 #     csvwriter = csv.writer(csvfile, delimiter=',')
-#     csvwriter.writerow(['Date', 'Profit'])
+#     csvwriter.writerow(['months])
         
